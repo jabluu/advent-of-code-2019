f7b3d73aa9af35cb4dbba94c6beac02291af1c77
jared <jaredl607@gmail.com>
2021-12-09 12:30:30 -0700

    day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -212,6 +212,156 @@ mod day02 {
 
         computer.run().program[0]
     }
+
+    pub fn part2() -> i32 {
+        #[derive(Clone)]
+        enum State {
+            EXEC,
+            HALT,
+        }
+
+        #[derive(Clone)]
+        struct Computer {
+            memory: Vec<i32>,
+            instruction_pointer: usize,
+            state: State,
+        }
+
+        impl Computer {
+            fn new(program: &[i32]) -> Self {
+                Self {
+                    memory: program.to_vec(),
+                    instruction_pointer: 0,
+                    state: State::EXEC,
+                }
+            }
+
+            fn with_inputs(&mut self, noun: i32, verb: i32) -> &mut Self {
+                self.memory[1] = noun;
+                self.memory[2] = verb;
+                self
+            }
+
+            fn execute_program(&mut self) -> &Self {
+                loop {
+                    match self.state {
+                        State::EXEC => {
+                            self.execute_instruction();
+                        },
+                        State::HALT => {
+                            break;
+                        }
+                    }
+                }
+
+                self
+            }
+
+            fn execute_instruction(&mut self) {
+                let instr_ptr = self.instruction_pointer;
+
+                let opcode = self.memory[instr_ptr];
+
+                match opcode {
+                    1 => {
+                        let param_addr = (
+                            self.memory[instr_ptr+1] as usize,
+                            self.memory[instr_ptr+2] as usize,
+                        );
+
+                        let result_addr = self.memory[instr_ptr+3] as usize;
+
+                        let param = (
+                            self.memory[param_addr.0],
+                            self.memory[param_addr.1],
+                        );
+
+                        let result = param.0 + param.1;
+                        self.memory[result_addr] = result;
+
+                        self.instruction_pointer += 4;
+                    },
+                    2 => {
+                        let param_addr = (
+                            self.memory[instr_ptr+1] as usize,
+                            self.memory[instr_ptr+2] as usize,
+                        );
+
+                        let result_addr = self.memory[instr_ptr+3] as usize;
+
+                        let param = (
+                            self.memory[param_addr.0],
+                            self.memory[param_addr.1],
+                        );
+
+                        let result = param.0 * param.1;
+                        self.memory[result_addr] = result;
+
+                        self.instruction_pointer += 4;
+                    },
+                    99 => {
+                        self.state = State::HALT;
+                        self.instruction_pointer += 1;
+                    },
+                    _ => panic!("unrecognized opcode: {}", opcode),
+                }
+            }
+        }
+
+        assert_eq!(
+            [2,0,0,0,99].to_vec(),
+            Computer::new(&[1,0,0,0,99]).execute_program().memory
+        );
+
+        assert_eq!(
+            [2,3,0,6,99].to_vec(),
+            Computer::new(&[2,3,0,3,99]).execute_program().memory
+        );
+
+        assert_eq!(
+            [2,4,4,5,99,9801].to_vec(),
+            Computer::new(&[2,4,4,5,99,0]).execute_program().memory
+        );
+
+        assert_eq!(
+            [30,1,1,4,2,5,6,0,99].to_vec(),
+            Computer::new(&[1,1,1,4,99,5,6,0,99]).execute_program().memory
+        );
+
+        use std::fs::File;
+        use std::io::{BufRead, BufReader};
+
+        let handle = File::open("input/day02/input.txt").unwrap();
+        let buffer = BufReader::new(handle);
+
+        let computers = buffer.lines()
+            .map(|line| {
+                line.unwrap().split(',').map(|item| {
+                    item.parse::<i32>().unwrap()
+                }).collect::<Vec<i32>>()
+            })
+            .map(|program| {
+                Computer::new(&program)
+            })
+            .collect::<Vec<Computer>>();
+
+        let computer = computers[0].clone();
+
+        for noun in 0..99 {
+            for verb in 0..99 {
+                let result = computer.clone()
+                    .with_inputs(noun, verb)
+                    .execute_program()
+                    .memory[0];
+
+                if result == 19690720 {
+                    return 100 * noun + verb;
+                }
+            }
+        }
+
+        0
+    }
 }
 
 fn main() {
